49679740df6a74dbec57e7c8062c688a3fc1af5d
Ibukun.Ogunsolu <ibukun.ogunsolu@interswitchgroup.com>
2020-03-06 17:04:44 +0100

    Updates

---
+++ precache-manifest.8874b0818c97ee8a5a50c05852e856f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2488dbcb0e15a06cd1fcf83303c66c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ee57173a312f9f4fe943",
+    "url": "/monsters-rolodex/static/css/main.07aca208.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee57173a312f9f4fe943",
+    "url": "/monsters-rolodex/static/js/main.4b8eb02b.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
