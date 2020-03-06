6897cd7375308df591aeef59b67c8f55ef1bb4fa
Ibukun.Ogunsolu <ibukun.ogunsolu@interswitchgroup.com>
2020-03-06 17:06:46 +0100

    adding pages for github deploy

---
+++ src/components/search-box/searchbox.component.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import './searchbox.style.css'
+
+export const SearchBox = ({placeholder, handleChange}) => {
+
+return <input className='search' type='search' placeholder={placeholder} onChange={handleChange}></input>
+
+}
